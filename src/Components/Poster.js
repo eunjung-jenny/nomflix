d4f4d08759ee96b4629d248a8ef722293369d573
jenny.kr <jenny.it.kr@gmail.com>
2020-05-22 02:42:33 +0900

    Add poster style

--- src/Components/Poster.js
+++ src/Components/Poster.js
@@ -3,17 +3,50 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-const Container = styled.div``;
+const Container = styled.div`
+  font-size: 12px;
+`;
 
-const ImageContainer = styled.div``;
+const Image = styled.div`
+  background-image: url(${(props) => props.bgUrl});
+  height: 180px;
+  background-size: contain;
+  background-repeat: no-repeat;
+  border-radius: 3px;
+  background-position: center center;
+  transition: opacity 0.1s linear;
+`;
 
-const Image = styled.div``;
+const Rating = styled.span`
+  position: absolute;
+  bottom: 5px;
+  right: 5px;
+  opacity: 0;
+  transition: opacity 0.15s linear;
+`;
 
-const Rating = styled.span``;
+const ImageContainer = styled.div`
+  position: relative;
+  margin-bottom: 5px;
+  &:hover {
+    ${Image} {
+      opacity: 0.3;
+    }
+    ${Rating} {
+      opacity: 1;
+    }
+  }
+`;
 
-const Title = styled.span``;
+const Title = styled.span`
+  display: block;
+  margin-bottom: 3px;
+`;
 
-const Year = styled.span``;
+const Year = styled.span`
+  font-size: 10px;
+  color: rgba(255, 255, 255, 0.5);
+`;
 
 const Poster = ({
   id,
@@ -26,7 +59,13 @@ const Poster = ({
   <Link to={isMovie ? `/movie/${id}` : `/show/${id}`}>
     <Container>
       <ImageContainer>
-        <Image bgUrl={imageUrl} />
+        <Image
+          bgUrl={
+            imageUrl
+              ? `https://image.tmdb.org/t/p/w300${imageUrl}`
+              : require("../assets/noImage.jpg")
+          }
+        />
         <Rating>
           <span role="img" aria-label="rating">
             ⭐️
--- src/Components/Section.js
+++ src/Components/Section.js
@@ -16,7 +16,7 @@ const Title = styled.span`
 const Grid = styled.div`
   margin-top: 15px;
   display: grid;
-  grid-template-columns: repeat(auto-fill, 200px);
+  grid-template-columns: repeat(auto-fill, 125px);
   grid-gap: 25px;
 `;
 
