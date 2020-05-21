6fb4d5bf7860d674c7e06708e37cb93e75aac8c7
jenny.kr <jenny.it.kr@gmail.com>
2020-05-22 03:53:40 +0900

    Updates

---
+++ precache-manifest.c0e16a2c9aea453c3f8bd7a9903f8407.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e78ca04707e986c8164b58b8769c57f2",
+    "url": "/nomflix/index.html"
+  },
+  {
+    "revision": "186ba9af5ea36d2ca2b9",
+    "url": "/nomflix/static/js/2.5395e6b9.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/nomflix/static/js/2.5395e6b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2df56dcbfeb159a4195",
+    "url": "/nomflix/static/js/main.0ed28233.chunk.js"
+  },
+  {
+    "revision": "e0fc413acc00e4e8c740",
+    "url": "/nomflix/static/js/runtime-main.8b8b03f7.js"
+  },
+  {
+    "revision": "e8779cd0af9840bd612feb1fab23c707",
+    "url": "/nomflix/static/media/noImage.e8779cd0.jpg"
+  }
+]);
